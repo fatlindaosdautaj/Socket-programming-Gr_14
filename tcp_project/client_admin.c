855094bf34e0bf34c7066ae543c9717bf5508f19
edonitagashi <edonita.gashi@student.uni-pr.edu>
2025-11-13 23:16:24 +0100

    client_admin.c

---
+++ tcp_project/client_admin.c
@@ -0,0 +1,38 @@
+#define _WIN32_WINNT 0x0600
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <process.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define SERVER_IP "172.20.10.12"
+#define SERVER_PORT 9999
+#define BUFFER_SIZE 4096
+#define DOWNLOADS_DIR "downloads"
+
+SOCKET client_socket;
+int running = 1;
+
+unsigned __stdcall receive_thread(void *arg) {
+    char buffer[BUFFER_SIZE];
+
+    while (running) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+
+        if (bytes_received <= 0) {
+            if (running) {
+                printf("\n[Connection lost]\n");
+                running = 0;
+            }
+            break;
+        }
+
+        buffer[bytes_received] = '\0';
+
+    }
+}
