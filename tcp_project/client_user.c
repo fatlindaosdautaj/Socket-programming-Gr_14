61e839a540958a9811685bae824e7a55bdd6e3d3
engjiosmani <154840265+engjiosmani@users.noreply.github.com>
2025-11-15 00:35:30 +0100

    update client user

--- tcp_project/client_user.c
+++ tcp_project/client_user.c
@@ -37,4 +37,36 @@ unsigned __stdcall receive_thread(void *arg)
         buffer[bytes_received] = '\0';
 
         printf("\n%s", buffer);
- 
+        if (buffer[strlen(buffer) - 1] != '\n')
+        {
+            printf("\n");
+        }
+        printf("> ");
+        fflush(stdout);
+    }
+
+    return 0;
+}
+
+unsigned __stdcall ping_thread(void *arg)
+{
+    while (running)
+    {
+        Sleep(60000);
+        if (running)
+        {
+            send(client_socket, "PING", 4, 0);
+        }
+    }
+    return 0;
+}
+
+void print_help()
+{
+    printf("\n=== USER COMMANDS ===\n");
+    printf("/read <filename>    - Read content of a file (slower than admin)\n");
+    printf("/help               - Show this help\n");
+    printf("/quit               - Exit client\n");
+    printf("Note: You can send any text message to the server\n");
+    printf("=====================\n\n");
+}
