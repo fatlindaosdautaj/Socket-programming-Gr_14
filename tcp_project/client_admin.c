dc26477fc18cf4358f54f9f1ed0bab931a12db7b
edonitagashi <edonita.gashi@student.uni-pr.edu>
2025-11-16 14:59:30 +0100

    client_admin main

--- tcp_project/client_admin.c
+++ tcp_project/client_admin.c
@@ -1,7 +1,6 @@
 #define _WIN32_WINNT 0x0600
 #include <winsock2.h>
 #include <ws2tcpip.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -152,4 +151,97 @@ void handle_upload(char *filename) {
     free(buffer);
 
     printf("Upload request sent...\n");
+}
+
+int main() {
+    WSADATA wsa;
+    struct sockaddr_in server_addr;
+
+    printf("=== TCP CLIENT (ADMIN) ===\n");
+    printf("Initializing...\n");
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("WSAStartup failed: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == INVALID_SOCKET) {
+        printf("Socket creation failed: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+
+    printf("Connecting to %s:%d...\n", SERVER_IP, SERVER_PORT);
+
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        printf("Connection failed: %d\n", WSAGetLastError());
+        printf("\nMake sure the server is running!\n");
+        closesocket(client_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Connected successfully!\n\n");
+
+    _beginthreadex(NULL, 0, receive_thread, NULL, 0, NULL);
+    _beginthreadex(NULL, 0, ping_thread, NULL, 0, NULL);
+
+    Sleep(500);
+
+    print_help();
+
+    char input[BUFFER_SIZE];
+
+    while (running) {
+        printf("> ");
+        fflush(stdout);
+
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;
+        }
+
+        input[strcspn(input, "\n")] = 0;
+
+        if (strlen(input) == 0) {
+            continue;
+        }
+
+        if (strcmp(input, "/help") == 0) {
+            print_help();
+            continue;
+        }
+
+        if (strcmp(input, "/quit") == 0) {
+            printf("Disconnecting...\n");
+            break;
+        }
+
+        if (strncmp(input, "/upload ", 8) == 0) {
+            handle_upload(input + 8);
+            continue;
+        }
+
+        strcat(input, "\n");
+        int sent = send(client_socket, input, strlen(input), 0);
+
+        if (sent == SOCKET_ERROR) {
+            printf("Send failed: %d\n", WSAGetLastError());
+            break;
+        }
+    }
+    running = 0;
+    closesocket(client_socket);
+    WSACleanup();
+
+    printf("Disconnected.\n");
+
+    return 0;
 }
